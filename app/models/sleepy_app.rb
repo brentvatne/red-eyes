24447bc36826a0f220b7cec37e74b75c849e2b44
Brent Vatne <brent.vatne@madriska.com>
2014-11-21 20:06:12 -0800

    Better url output

--- app/models/sleepy_app.rb
+++ app/models/sleepy_app.rb
@@ -1,4 +1,14 @@
 class SleepyApp < ActiveRecord::Base
   scope :active, -> { where(active: true) }
   scope :deactivated, -> { where(active: [false, nil]) }
+
+  def url
+    base = read_attribute(:url)
+
+    if base.match(/http/)
+      base
+    else
+      "http://#{base}"
+    end
+  end
 end
